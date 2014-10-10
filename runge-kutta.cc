60ad62391a9aa2cd686ff25bf2aa16656c947208
Joshua Wallace <joshuajw@irene.astro.Princeton.EDU>
2014-10-10 16:58:12 -0400

    rk4_started

--- runge-kutta.cc
+++ runge-kutta.cc
@@ -13,18 +13,70 @@ Runge_Kutta::~Runge_Kutta(){
 }
 
 int Runge_Kutta::Step(double t, double *x){
+
+  double k[dimen_][4]; //to store the k values as we go along
   model_.rhs(t,x,fx_);
-  for(int i = 0; i < dimen_; i++) {
-    double k[4]; //the corresponding 4 k values used in Runge_Kutta method
-    double xtofeedin; //to store the x values we will need to use to find the k's
-    //first, calculate k1
-    k[0]=fx[i];
-
-    //find the x value we need to plug in to find k2
-    xtofeedin=x[i] + dt_/2. * k[0];
-
-    //now find k2, first evaluate rhs then get k2
-    model_.rhs(t,x,fx)
-  }
+  
+  //calculates k1, based on fx_ above
+  for(int i=0;i<dimen_;i++)
+    {
+      k[i][0]=fx_[i];
+    }
+  
+  //allows us to set x to feed in to the rhs function as x + h/2 * k_1
+  //since k_1 is going to be different for each of the n elements of x
+  double xtofeedin[dimen_];
+  
+  for(int i=0;i<dimen_;i++)
+    {
+      xtofeedin[i]=x[i]+dt_/2 * k[i][0];
+    }
+  
+  model_.rhs(t+(*integrator).dt_/2.,xtofeedin,fx);
+  
+  //calculates k2, based on fx above, which was based on xtofeedin above
+  for(int i=0;i<dimen_;i++)
+    {
+      k[i][1]=fx[i];
+    }
+  
+  
+  //Now for k_3
+  //First, xtofeedin
+  for(int i=0;i<dimen_;i++)
+    {
+      xtofeedin[i]=x[i]+dt_/2. * k[i][1];
+    }
+  
+  model_.rhs(t+dt_/2.,xtofeedin,fx);
+  
+  //calculates k3, based on fx above, which was based on xtofeedin above
+  for(int i=0;i<dimen_;i++)
+    {
+      k[i][2]=fx[i];
+    }
+  
+  
+  //Now for k_4
+  //First, xtofeedin
+  for(int i=0;i<dimen_;i++)
+    {
+      xtofeedin[i]=x[i]+dt_ * k[i][2];
+    }
+  
+  model_.rhs(t+dt_,xtofeedin,fx);
+  
+  //calculates k3, based on fx above, which was based on xtofeedin above
+  for(int i=0;i<dimen_;i++)
+    {
+      k[i][3]=fx[i];
+    }
+  
+  /////////////////Now to actually update the x values
+  
+  for(int i=0;i<dimen_;i++)
+    {
+      x[i] = x[i] + dt_/6.*k[i][0] + dt_/3.*(k[i][1] + k[i][2]) + dt_/6.*k[i][3];
+    }
   return 0;
 }
