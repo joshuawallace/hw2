88dcb5d0731f289acb87b5356ce2fc22f638123b
Joshua Wallace <joshuajw@princeton.edu>
2014-10-13 13:03:48 -0400

    messing with ode solver more

--- ode_solve.cc
+++ ode_solve.cc
@@ -34,7 +34,7 @@ int main(int argc, char **argv){
 
   //  const char *equation_name[120];
   //  const char *integrator_name[120];
-
+  const double *initial_conditions;
 
   if(argc != 5) {
     intromessage(argv[0]);
@@ -42,32 +42,49 @@ int main(int argc, char **argv){
 
   const double dt = atof(argv[3]);
   const int nsteps = atoi(argv[4]);
+  Model *model;
 
   if(argv[1] == "duffing"){
     const double delta = 0.2;
     const double gamma = 0.3;
     const double omega = 1.0;
-    Model *model = new Duffing(delta, gamma, omega);
+    //    *initial_conditions = new (0.0, 0.0);
+    model = new Duffing(delta, gamma, omega);
+    double x[model.dimen()];
+    x[0]=0.0;
+    x[1]=0.0;
   }
 
   else if(argv[1] == "lorenz"){
     const double sigma = 10.;
     const double rho = 28.;
     const double beta = 8./3.;
-    Model *model = new Lorenz(sigma, rho, beta);
+    model = new Lorenz(sigma, rho, beta);
+    double x[model.dimen()];
+    x[0]=0.0;
+    x[1]=0.1;
+    x[0]=0.0;
   }
 
   else if(argv[1] == "linear-oscillator"){
     const double beta = 0.1;
     const double gamma = 1.;
     const double omega = 0.9;
-    Model *model = new LinearOscillator(beta, gamma, omega);
+    model = new LinearOscillator(beta, gamma, omega);
+    double x[model.dimen()];
+    x[0]=0.0;
+    x[0]=0.0;
   }
 
   else{
     printf("Didn't recognize the EQUATION name you entered, which was %s\nNow exiting...\n",argv[1]);
     return -1;
   }
+
+
+
+
+
 
   if(argv[2] == "euler"){
     Integrator *integrator = new Euler(dt, *model);
@@ -88,7 +105,7 @@ int main(int argc, char **argv){
   }
 
   double t=0.;
-  PrintState(dimen, t, x);
+  PrintState(model.dimen(), t, x);
   for(int i = 0; i < nsteps; ++i){
     integrator->Step(t, x);
     t = (i+1) * dt;
