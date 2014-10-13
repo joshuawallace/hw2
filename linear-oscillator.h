54470cd268fa59095e23858918c3089efde2d6cf
Joshua <joshuawallace800@gmail.com>
2014-10-12 21:56:47 -0400

    Part 2 done

---
+++ linear-oscillator.h
@@ -0,0 +1,22 @@
+#ifndef LINEAR_OSCILLATOR_H_
+#define LINEAR_OSCILLATOR_H_
+
+#include "model.h"
+
+// Forced LinearOscillator oscillator:
+//   \dot x = y
+//   \dot y = - x  - 2 * beta * y + gamma * cos(omega t)
+class LinearOscillator : public Model {
+ public:
+  LinearOscillator(double beta, double gamma, double omega);
+  ~LinearOscillator();
+  int rhs(double t, const double *x, double *fx) const;
+  int dimen() const { return kDimen; }
+ private:
+  const double beta_;                         // damping constant (I think)
+  const double gamma_;                        // amplitude of forcing
+  const double omega_;                        // frequency of forcing
+  static const int kDimen = 2;
+};
+
+#endif  // LINEAR_OSCILLATOR_H_
---
+++ lorenz.h
@@ -0,0 +1,23 @@
+#ifndef LORENZ_H_
+#define LORENZ_H_
+
+#include "model.h"
+
+// Lorenz oscillator:
+//   \dot x = sigma * (y - x)
+//   \dot y = rho * x - y - x * z
+//   \dot z = -beta * z + x * y
+class Lorenz : public Model {
+ public:
+  Lorenz(double sigma, double rho, double beta);
+  ~Lorenz();
+  int rhs(double t, const double *x, double *fx) const;
+  int dimen() const { return kDimen; }
+ private:
+  const double sigma_;                        // I honestly don't know
+  const double rho_;                          // what each of these
+  const double beta_;                         // constants mean
+  static const int kDimen = 3;
+};
+
+#endif  // LORENZ_H_
